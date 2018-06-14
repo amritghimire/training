d139aff91a647ef127525c0ee7a77a6a30815bdb
Amrit Ghimire <iamritghimire@gmail.com>
2018-06-14 08:06:10 +0545

    Advance views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+
+def index(request):
+    all_post = Post.objects.all()
+    output = ', '.join([ post.title for post in all_post ])
+    return HttpResponse("You are looking at homepage. The posts are %s."%output)
+
+def detail(request, post_id):
+    return HttpResponse("You're looking at detail page for post id  %s." % post_id)
+
