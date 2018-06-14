672732146b09cc744bee09a872584ab86f1e5051
Amrit Ghimire <iamritghimire@gmail.com>
2018-06-14 08:11:47 +0545

    Urls.py

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex: /
+    path('', views.index, name='index'),
+    # ex: /posts/5/
+    path('posts/<int:post_id>/', views.detail, name='detail'),
+]
