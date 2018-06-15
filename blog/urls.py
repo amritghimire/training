d7a1d09e80bea537c80f0c3da1cf5b65ada44c17
Amrit Ghimire <iamritghimire@gmail.com>
2018-06-15 09:53:25 +0545

    edit_form

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ex: /posts/5/
     path('posts/<int:post_id>/', views.detail, name='detail'),
+    path('posts/edit/<int:post_id>/', views.post_edit, name='edit'),
     path('posts/new',views.post_new,name="new_post")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
@@ -14,6 +14,29 @@ def detail(request, post_id):
     return render(request,'blog/post_detail.html',{'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('detail', post_id=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('detail', post_id=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
