1552d66b6b63d6e07ea867b56467035155025a09
Amrit Ghimire <iamritghimire@gmail.com>
2018-06-15 09:07:50 +0545

    display posts and view

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 from .models import Post
 # Create your views here.
 
 def index(request):
-    all_post = Post.objects.all()
-    output = ', '.join([ post.title for post in all_post ])
-    return render(request,'blog/post_list.html',{})
+    all_post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/post_list.html',{'posts':all_post})
 
 def detail(request, post_id):
     return HttpResponse("You're looking at detail page for post id  %s." % post_id)
