0cb942371bde19becfe7c48eeb0a876938866230
Amrit Ghimire <iamritghimire@gmail.com>
2018-06-15 09:40:22 +0545

    urls.py new posts

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ex: /posts/5/
     path('posts/<int:post_id>/', views.detail, name='detail'),
+    path('posts/new',views.post_new,name="new_post")
 ]
